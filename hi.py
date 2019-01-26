1e1d23b04488ebbb7d4004ed084e4d809ae9147b
kiwee <f6985236@gmail.com>
2019-01-26 23:49:56 +0800

    add new file

---
+++ hi.py
@@ -0,0 +1,7 @@
+name = input('enter ur name')
+print("hi,beauty",name)
+input("what's ur height?")
+print("ohhhh!you're so tall! such a fantasy girl")
+
+input("what's ur weight?")
+print("are u kidding me? you're a model!")
